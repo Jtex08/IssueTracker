2610fe3c6def2d2167941e2b1840a212e63bd096
Jonathan Noland <jnoland08@tamu.edu>
2020-01-27 20:43:47 -0600

    Add project files.

---
+++ IssueTracker/Pages/Settings/Account.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IssueTracker.Data;
+using IssueTracker.Services.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IssueTracker.Pages.Settings
+{
+    public class AccountModel : PageModel
+    {
+        public AccountModel() 
+        {
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
---
+++ IssueTracker/Pages/Settings/History.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IssueTracker.Data;
+using IssueTracker.Services.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IssueTracker.Pages.Settings
+{
+    public class HistoryModel : PageModel
+    {
+        public HistoryModel() 
+        {
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
