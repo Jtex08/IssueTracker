342f3e4312afb7f31d1ac41b19dab4d219af8d1e
Jonathan Noland <jnoland08@tamu.edu>
2020-03-16 17:03:31 -0500

    Details failing to display properly

---
+++ IssueTracker/Data/Migrations/20200316211625_Tickets.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IssueTracker.Data.Migrations
+{
+    public partial class Tickets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerUserId",
+                table: "Projects");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerUserId",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
